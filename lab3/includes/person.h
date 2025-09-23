19c6d7342142cb8ced922577f39cb0e6f8fa97d1
kiribu <kiryshabutor@gmail.com>
2025-09-23 13:00:29 +0300

    fix sonar

--- lab3/includes/person.h
+++ lab3/includes/person.h
@@ -7,7 +7,7 @@ private:
     std::string firstName;
     std::string lastName;
     std::string middleName;
-    int birthYear;
+    int birthYear{0};
 
 public:
     Person();
@@ -15,15 +15,20 @@ public:
            const std::string& m, int y);
     virtual ~Person() = default;
 
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getMiddleName() const;
-    int getBirthYear() const;
+    Person(const Person& other) = default;
+    Person& operator=(const Person& other) = default;
+    Person(Person&& other) noexcept = default;
+    Person& operator=(Person&& other) noexcept = default;
 
     void setFirstName(std::string_view f);
     void setLastName(std::string_view l);
     void setMiddleName(std::string_view m);
     void setBirthYear(int y);
+
+    std::string_view getFirstName() const;
+    std::string_view getLastName() const;
+    std::string_view getMiddleName() const;
+    int getBirthYear() const;
 
     virtual void inputData();
     virtual void printInfo() const;
